dd51c955326433b724d2b8203a67909c0d2c1e76
codeclout <bduckett@codeclout.com>
2022-04-24 20:59:19 -0400

    task - ActivateOrganization sig

--- gateway/framework/out/db/organization.go
+++ gateway/framework/out/db/organization.go
@@ -8,10 +8,10 @@ import (
 )
 
 type OrganizationRepository interface {
-	ActivateOrganization(ctx context.Context, id uuid.UUID) error
+	ActivateOrganization(id uuid.UUID) error
 	DeactivateOrganization(ctx context.Context, id uuid.UUID) error
 	GetOrganization(ctx context.Context, id uuid.UUID) (model.Details, error)
 	GetOrganizationBatch(ctx context.Context, ids []uuid.UUID) ([]model.Details, error)
-	LogOrganizationHistoryEvent(ctx context.Context, event model.OrganizationEvent) error
+	LogOrganizationHistoryEvent(ctx context.Context) error
 	UpsertOrganizationUnit(ctx context.Context, unit model.Details) error
 }
