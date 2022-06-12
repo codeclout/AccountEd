6d139b511ab500716e4ad45bde4b764727d7bfca
codeclout <bduckett@codeclout.com>
2022-06-12 14:58:14 -0400

    committing changes

--- backend/gateway/core/organization/organization.go
+++ backend/gateway/core/organization/organization.go
@@ -7,6 +7,15 @@ import (
 	"github.com/google/uuid"
 )
 
+type EventType byte
+
+const (
+	_                      = iota
+	EventSuccess EventType = iota
+	EventFailed
+	EventError
+)
+
 type OrganizationPort interface {
 	ActivateOrganization(ctx context.Context, in ActivateInput) error
 	DeactivateOrganization(ctx context.Context, id uuid.UUID) error
@@ -15,17 +24,28 @@ type OrganizationPort interface {
 	LogOrganizationHistoryEvent(ctx context.Context) error
 	UpsertOrganizationUnit(ctx context.Context, unit Details) error
 }
+
 type ActivateInput struct {
 	Date  time.Time
 	Email string
 	ID    uuid.UUID
 	Name  string
 }
+
 type Details struct {
 	ID    uuid.UUID
 	Name  string
 	Units []OrganizationUnit
 }
+
+type OrganizationEvent struct {
+	CreatedAt time.Time
+	EventID   uuid.UUID
+	EventType EventType
+	Key       string
+	Value     string
+}
+
 type CreateOrganizationUnitInput struct {
 	Name     string
 	ParentID string
---
+++ migrations/mongo/migration/migrations/20220611033222-membership.js
@@ -0,0 +1,56 @@
+const { ObjectId, Double } = require("mongodb");
+
+module.exports = {
+  async up(db, client) {
+    await db.createCollection("membership");
+    await db.createCollection("counters", {
+      collation: {
+        caseLevel: true,
+        locale: "en_US",
+        numericOrdering: true,
+        strength: 2,
+      },
+    });
+
+    await db.collection("counters").insertOne({ name: "membership", sid: 0 });
+
+    const ac = await db
+      .collection("account_type")
+      .find({ type: "homeschooler" })
+      .toArray();
+    const homeschoolerAccountTypeId = new ObjectId(ac[0]._id);
+
+    const initialData = [
+      {
+        account_type: homeschoolerAccountTypeId,
+        active: true,
+        created_at: new Date(Date.now()),
+        expires_at: new Date(Date.now() + 31556952000),
+        price: new Double(20.0),
+        sequence_id: null,
+      },
+    ];
+
+    await db.collection("membership").insertMany(initialData);
+
+    const membershipCounter = await db
+      .collection("counters")
+      .findOneAndUpdate(
+        { name: "membership" },
+        { $inc: { sid: 1 } },
+        { returnDocument: "after" }
+      );
+
+    await db
+      .collection("membership")
+      .updateOne(
+        { account_type: homeschoolerAccountTypeId },
+        { $set: { sequence_id: membershipCounter.value.sid } }
+      );
+  },
+
+  async down(db, client) {
+    await db.collection("membership").drop();
+    await db.collection("counters").drop();
+  },
+};
