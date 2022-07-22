aaaefb4d66eb071e1597776fbf2279fd36578288
codeclout <bduckett@codeclout.com>
2022-07-21 20:26:30 -0400

    task - create ecr repo

--- infrastructure/modules/compute/ecr/main.tf
+++ infrastructure/modules/compute/ecr/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_ecr_repository" "container_repo" {
   name                 = "${var.app}-${var.environment}"
   image_tag_mutability = var.image_tag_mutability
