2ed72cb9a4c617d4435d6a1ec33355e5bde857ba
codeclout <bduckett@codeclout.com>
2023-09-25 22:26:08 -0400

    @task - update services and pkg dependencies

--- session/ports/framework/drivers/session.go
+++ session/ports/framework/drivers/session.go
@@ -1,13 +1,11 @@
 package driver
 
 import (
-  "context"
-  "time"
-
-  sessiontypes "github.com/codeclout/AccountEd/session/session-types"
+	"context"
+	"time"
 )
 
 type SessionPort interface {
-  CreateToken(ctx context.Context, duration time.Duration, groupid, memberid string) (*string, error)
-  VerifyTokenData(ctx context.Context) (*sessiontypes.StatelessAPI, error)
+	CreateToken(ctx context.Context, duration time.Duration, groupid, memberid string) (*string, error)
+	// VerifyTokenData(ctx context.Context) (*sessiontypes.StatelessAPI, error)
 }
