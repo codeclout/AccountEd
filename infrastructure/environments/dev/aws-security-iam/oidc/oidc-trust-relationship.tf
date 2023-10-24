9dca854ba4e687dd8686e282352c107554f41a5b
codeclout <bduckett@codeclout.com>
2023-10-24 14:55:17 -0400

    fix - formatting

--- infrastructure/environments/dev/aws-security-iam/oidc/oidc-trust-relationship.tf
+++ infrastructure/environments/dev/aws-security-iam/oidc/oidc-trust-relationship.tf
@@ -1,51 +1,47 @@
 resource "aws_iam_role" "oidc_role_tfc" {
   name = "oidc-role"
 
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-           "Effect": "Allow",
-           "Principal": {
-             "Federated": "${module.oidc_terraform_cloud.openid-connect-provider-arn}"
-           },
-           "Action": "sts:AssumeRoleWithWebIdentity",
-           "Condition": {
-             "StringEquals": {
-               "${module.oidc_terraform_cloud.openid-connect-provider-hostname}:aud": "${one(module.oidc_terraform_cloud.openid-connect-provider-client-id-list)}"
-             },
-             "StringLike": {
-               "${module.oidc_terraform_cloud.openid-connect-provider-hostname}:sub": "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
-             }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect : "Allow",
+        Principal : {
+          Federated : module.oidc_terraform_cloud.openid-connect-provider-arn
+        },
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Condition : {
+          StringEquals : {
+            "${module.oidc_terraform_cloud.openid-connect-provider-hostname}:aud" : one(module.oidc_terraform_cloud.openid-connect-provider-client-id-list)
+          },
+          StringLike : {
+            "${module.oidc_terraform_cloud.openid-connect-provider-hostname}:sub" : "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
           }
         }
-      ]
-    }
-  EOF
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "oidc_role_github" {
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Federated": "${module.oidc_github.openid-connect-provider-arn}"
-          },
-          "Action": "sts:AssumeRoleWithWebIdentity",
-          "Condtion": {
-            "StringEquals": {
-              "${module.oidc_github.openid-connect-provider-hostname}:aud": "${one(module.oidc_github.openid-connect-provider-client-id-list)}"
-              "${module.oidc_github.openid-connect-provider-hostname}:sub": [
-                "repo:codeclout/AccountEd:ref:refs/heads/alpha
-              ]
-            }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect : "Allow",
+        Principal : {
+          Federated : module.oidc_github.openid-connect-provider-arn
+        },
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Condition : {
+          StringEquals : {
+            "${module.oidc_github.openid-connect-provider-hostname}:aud" : one(module.oidc_github.openid-connect-provider-client-id-list)
+            "${module.oidc_github.openid-connect-provider-hostname}:sub" : [
+              "repo:codeclout/AccountEd:ref:refs/heads/alpha"
+            ]
           }
         }
-      ]
-    }
-  EOF
+      }
+    ]
+  })
 }
