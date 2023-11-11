3859581abb6cf3fa18155dbdab4c36edadf54553
codeclout <bduckett@codeclout.com>
2023-11-11 09:33:07 -0500

    task - add support for gitlab

--- infrastructure/environments/dev/aws-security-iam/oidc/security.tf
+++ infrastructure/environments/dev/aws-security-iam/oidc/security.tf
@@ -15,5 +15,6 @@ module "oidc_terraform_cloud" {
 module "oidc_gitlab" {
   source = "../../../../modules/security/oidc"
 
+  identity_provider_audience = "https://gitlab.com"
   identity_provider_url = "gitlab.com"
 }
--- infrastructure/modules/security/oidc/variables.tf
+++ infrastructure/modules/security/oidc/variables.tf
@@ -1,6 +1,5 @@
 variable "identity_provider_audience" {
   type = string
-  default = ""
 }
 
 variable "identity_provider_url" {
